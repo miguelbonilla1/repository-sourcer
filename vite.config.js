18c8d2a9679f137a2a105fd62ca09d882b40a5ea
Miguel Bonilla <miguelbonilla1886@gmail.com>
2024-11-02 12:41:51 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Reposourcer/' // Reemplaza "nombre-del-repo" con el nombre real de tu repositorio
-})
+  base: "/repository-sourcer/", // Reemplaza "nombre-del-repo" con el nombre real de tu repositorio
+});
